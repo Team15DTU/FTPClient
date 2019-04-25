a2585e5f91b250156e0b5420dd726efae901d3ca
Anders <s185100@student.dtu.dk>
2019-04-25 09:24:54 +0200

    Ny måde at lave threads på.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,19 @@ import java.net.UnknownHostException;
 public class Main {
     public static void main(String[] args) throws UnknownHostException {
         String hostName = "ftp.cs.brown.edu";
+        FTPComs ftpComs = new FTPComs(hostName);
+        DataConn dataConn = new DataConn(hostName);
+
+        Thread t1 = new Thread(ftpComs);
+        Thread t2 = new Thread(dataConn);
+
+        t1.start();
+        t2.start();
+
+        /*
         (new Thread(new FTPComs(hostName))).start();
         (new Thread(new DataConn(hostName))).start();
+        */
     }
 
 }
