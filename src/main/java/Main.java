707ab2a2276ea513bcc18a75decab84594fe9c44
Anders <s185100@student.dtu.dk>
2019-05-01 14:23:37 +0200

    Lidt cleanup i Main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,11 @@ import java.net.UnknownHostException;
 public class Main {
     public static void main(String[] args) throws UnknownHostException {
         String hostName = "ftp.cs.brown.edu";
-        FTPComs ftpComs = new FTPComs(hostName);
-        DataConn dataConn = new DataConn(hostName);
-
-        Thread t1 = new Thread(ftpComs);
-        Thread t2 = new Thread(dataConn);
-
-        t1.start();
-        t2.start();
-
-        /*
         (new Thread(new FTPComs(hostName))).start();
         (new Thread(new DataConn(hostName))).start();
-        */
     }
-
 }
+
 //FTPComs ftpComs = new FTPComs(InetAddress.getByAddress(ip));
 //FTPComs ftpComs = new FTPComs("ftp.cs.brown.edu");
 //DataConn dataConn = new DataConn();
